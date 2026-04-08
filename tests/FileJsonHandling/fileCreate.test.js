80f71d3e16bc9926e339d765d340ddc855fbfb02
Santhoshkumar Sankar <Santhoshkumar.Sankar@ATMECS.CORP>
2026-04-08 17:54:31 +0530

    Json file create test

---
+++ tests/FileJsonHandling/fileCreate.test.js
@@ -0,0 +1,7 @@
+
+import fs from 'fs/promises'
+//await fs.writeFile('./output.txt', 'Hello');
+await fs.writeFile('C:/Users/Santhoshkumar.Sankar/Desktop/output.txt', 'Test Passed');
+
+console.log("Data written")
+
